23f9bf10360dea2bccd04300b6fbcae6c6e01a91
JHuardC <joehuard2711@gmail.com>
2023-09-15 21:04:38 +0100

    _settings.py set up to load config.yaml

--- src/hikeplanner/_settings.py
+++ src/hikeplanner/_settings.py
@@ -1,19 +1,12 @@
 """
 Loads Key Constants used by Hike Planner UK Project.
 """
-import os
-from typing import Final
 from pathlib import Path
-from dotenv import load_dotenv
+import yaml
 
-load_dotenv()
+with open(Path(__file__).absolute().parent.joinpath('config.yaml'), 'r') as f:
+    presets: dict[str, dict[str, str | int] | str | int] = yaml.safe_load(f)
 
-# Google Maps API key
-with open(Path(os.getenv('MAPS_API_KEY_PATH')), 'r') as f:
-    GOOGLE_MAPS_API_KEY: Final[str] = f.read()
 
-# Project Path
-PROJECT_PATH: Final[Path] = Path(os.getenv('PROJECT_DIR'))
-
-if __name__ == "__main__":
-    print(f'{PROJECT_PATH = }', f'{GOOGLE_MAPS_API_KEY =}', sep = '\n')
+if __name__ == '__main__':
+    print(presets)
